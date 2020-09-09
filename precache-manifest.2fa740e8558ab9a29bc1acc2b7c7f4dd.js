cf9eafbe9aa39e93ebc4ac09cce4dbedf0abf09c
sjadriana <adriana.sj@live.com>
2020-09-09 08:31:16 -0300

    Updates

---
+++ precache-manifest.2fa740e8558ab9a29bc1acc2b7c7f4dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e016a35e3d7f5a5f84982968b21429d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0b419aa61c9d2584ee1",
+    "url": "./static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "dc9f3a90b5ff738f3552",
+    "url": "./static/js/2.8f68b7e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8f68b7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b419aa61c9d2584ee1",
+    "url": "./static/js/main.e6ba0de5.chunk.js"
+  },
+  {
+    "revision": "6f4948b1a828fb4f07c9",
+    "url": "./static/js/runtime-main.7a668861.js"
+  }
+]);
