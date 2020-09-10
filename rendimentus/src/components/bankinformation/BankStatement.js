8cd81fa99d7a0fcbd5176482f071e42199e19383
jessicamelise <jessica.melise@hotmail.com>
2020-09-09 22:55:43 -0300

    feat(statement): initial implementation

--- rendimentus/src/components/bankinformation/BankStatement.js
+++ rendimentus/src/components/bankinformation/BankStatement.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from "react-router-dom";
+import './bankinformation.css';
+
+const loadApiGetBanks = async () => {
+  try {
+    const getBanksApi = await fetch("https://jsonbox.io/box_ddb0ab5da8d69da8c315/banks")
+    const response = await getBanksApi.json();
+    return response;
+  }
+  catch (err) {
+    console.error("We got a problem to fetch the information", err)
+  }
+}
 
 export const BankStatement = () => {
   let history = useHistory();
+  const [extract, setExtract] = useState("");
 
   const handleClick = (path) => {
     history.push(path);
   };
 
+  useEffect(() => {
+    loadApiGetBanks().then((client) => setExtract(client[2].accounts[0].accountExtract));
+  }, []);
 
   return (
     <>
-    <header></header>
-    <main>Olá</main>
+    <section>
+      <div className="section"></div>
+    </section>
     <footer>
       <div onClick={() => handleClick("/statement")}>extrato</div>
       <div onClick={() => handleClick("/invoice")}>fatura</div>
