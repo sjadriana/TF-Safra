a072fa582ccb20b64a3a69f21fc09b5eae6237b3
jessicamelise <jessica.melise@hotmail.com>
2020-09-11 08:39:43 -0300

    fix:css footer

--- rendimentus/src/components/footer/index.js
+++ rendimentus/src/components/footer/index.js
@@ -1,31 +1,65 @@
 import React from 'react';
 import Images from '../images/images.js';
-import Invoice from '../../assests/invoice.svg'
-import Card from '../../assests/card.svg'
-import Contact from '../../assests/contact.svg'
-import { useHistory, useLocation } from "react-router-dom"
+import Invoice from '../../assests/invoice.svg';
+import Card from '../../assests/card.svg';
+import Contact from '../../assests/contact.svg';
+import InvoiceBlue from '../../assests/invoice-blue.svg';
+import CardBlue from '../../assests/card-blue.svg';
+import ContactBlue from '../../assests/contact-blue.svg';
+import { useHistory, useLocation } from "react-router-dom";
 import './footer.css'
 
  const Footer = () =>{
   let history = useHistory();
   let location = useLocation();
 
+  const background = (path) => {
+    return location.pathname === path;
+  }
+
   const handleClick = (path) => {
     history.push(path);
   };
    return (
-     <footer>
-       <div onClick={() => handleClick(`/statement/${location.pathname.split("/")[2]}`)}>
-         <Images src={Invoice} class='footer-images' />
-         <Images src={Invoice} class='footer-images imgs' />
+     <footer className="footer">
+       <div 
+          onClick={() => handleClick(`/statement/${location.pathname.split("/")[2]}`)}
+          className={background(`/statement/${location.pathname.split("/")[2]}`) 
+            ? "footer-background-white div-footer" 
+            : "footer-background-blue div-footer"}
+        >
+         <Images 
+          class='footer-images'
+          src={background(`/statement/${location.pathname.split("/")[2]}`) ?
+          InvoiceBlue :
+          Invoice}  
+          />
        </div>
-       <div onClick={() => handleClick(`/invoice/${location.pathname.split("/")[2]}`)} >
-         <Images src={Card}class='footer-images' />
-         <Images src={Card}class='footer-images imgs' />
+       <div 
+          onClick={() => handleClick(`/invoice/${location.pathname.split("/")[2]}`)}
+          className={background(`/invoice/${location.pathname.split("/")[2]}`) 
+            ? "footer-background-white div-footer" 
+            : "footer-background-blue div-footer"} 
+        >
+         <Images 
+          class='footer-images'
+          src={background(`/invoice/${location.pathname.split("/")[2]}`) ?
+          CardBlue :
+          Card} 
+          />
        </div>
-       <div onClick={() => handleClick(`/contact/${location.pathname.split("/")[2]}`)}>
-         <Images src={Contact}class='footer-images' />
-         <Images src={Contact}class='footer-images imgs' />
+       <div 
+          onClick={() => handleClick(`/contact/${location.pathname.split("/")[2]}`)}
+          className={background(`/contact/${location.pathname.split("/")[2]}`) 
+            ? "footer-background-white div-footer" 
+            : "footer-background-blue div-footer"}
+          >
+         <Images 
+          class='footer-images'
+          src={background(`/contact/${location.pathname.split("/")[2]}`) ?
+          ContactBlue :
+          Contact} 
+          />
        </div>
      </footer>
    )
