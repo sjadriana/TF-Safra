a16847efeab67d9d5fa841e33282d4568003b3ef
jessicamelise <jessica.melise@hotmail.com>
2020-09-11 00:28:38 -0300

    fix:adjust css bank information

--- rendimentus/src/components/bankinformation/BankInvoice.js
+++ rendimentus/src/components/bankinformation/BankInvoice.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './bankinformation.css';
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import { Header } from "../header/index.js";
 import { loadApiGetBanks } from "./loadApi.js";
 import Footer from '../footer';
+import Images from '../images/images.js';
+import Card from '../../assests/card.svg';
+
 
 export const BankInvoice = () => {
   const [invoice, setInvoice] = useState([]);
   let location = useLocation();
+  let history = useHistory();
 
   useEffect(() => {
     loadApiGetBanks().then((client) => {
@@ -19,18 +23,21 @@ export const BankInvoice = () => {
     return invoice.reduce((total, currentValue) => total += currentValue.value,0)
   }
 
+  const handleClick = (path) => {
+    history.push(path);
+  };
+
   return (
     <>
       <Header />
-      <h1 className="hExtract">Fatura</h1>
+      <h1 className="h-extract">Fatura</h1>
       <section className="section">
-        <div className="divExtract">
-          <p>Total: R${sumExtract().toFixed(2)}</p>
-          {invoice.map((eachValue, index) => (
-            <div key={index}>
-              <p className="pExtract">Valor: R${eachValue.value}</p>
-            </div>
-          ))}
+        <div className="div-extract text-center">
+          <Images class="icon-card" src={Card} />
+          <p className="p-size">R$ {sumExtract().toFixed(2)}</p>
+        </div>
+        <div className="div-extract p-margin see-statement">
+          <p onClick={() => handleClick(`/statement/${location.pathname.split("/")[2]}`)}>Ver Extrato Completo</p>
         </div>
       </section>
       <Footer />
