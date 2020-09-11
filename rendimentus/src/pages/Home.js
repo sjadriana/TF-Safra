9e9c628153756634a648620dbbceff7a725b9da6
jessicamelise <jessica.melise@hotmail.com>
2020-09-10 23:13:55 -0300

    feat(login):adjust css and route

--- rendimentus/src/pages/Home.js
+++ rendimentus/src/pages/Home.js
@@ -1,34 +1,20 @@
 import React from "react";
-import { useHistory} from "react-router-dom";
 import Form from "../components/form/Form.js";
 import logo from "../components/form/logo-safra-gold.svg";
 import "./Home.css"
 
 
 export const Home = () => {
-    let history = useHistory();
-    // const handleClick = () => {
-    //     history.push("/profile")
-    // }
-
-    // const [login, setLogin] = useState();
-
-
-
 
     return (
         <main className='main-home'>
             <div className='container-home'>
-                <img src={logo} alt='' className='logo'></img>
-                <h2 className='gold'>Rendimentus</h2>
-                <section>
-                    <div>
-                        <ul>CPF/CNPJ</ul>
-                    </div>
+                <img src={logo} alt='' className='logo-home'></img>
+                <p className='gold'>Rendimentus</p>
+                <section className="section-cpf">
+                    <p className="cpf-cnpj">CPF/CNPJ</p>
                 </section>
-                <Form
-                    // onChangeLogin={(event) => setLogin(event.target.value)}
-                />
+                <Form />
             </div>
         </main>
     )
