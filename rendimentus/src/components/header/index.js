ee1eeee25b487d6e7674383ea72b6dfe1b74c6e9
renataalmeids <ras_almeida@hotmail.com>
2020-09-11 01:24:41 -0300

    feat(profile): hidden btn

--- rendimentus/src/components/header/index.js
+++ rendimentus/src/components/header/index.js
@@ -4,29 +4,33 @@ import logo from '../../assests/logo-safra.svg'
 import logout from '../../assests/logout.svg'
 import back from '../../assests/back.svg'
 import './header.css'
-import { useHistory } from "react-router-dom"
+import { useHistory, useLocation } from "react-router-dom"
+
 
 export const Header = () => {
   const [name, setName] = useState([]);
   const [cpf, setCpf] = useState([]);
+  const location = useLocation();
+  const compareLocation = location.pathname === "/profile"
+
   useEffect(() => {
     fetch(`https://jsonbox.io/box_ddb0ab5da8d69da8c315/client`)
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (data) {
-      let name = data.map(item => item.name).reduce((acc, client) => {
-        acc += `<p>${client.name}</p>`
-        return acc
+      .then(function (response) {
+        return response.json()
       })
-      name = name.split(' ')[0]
-      setName(name)
-      const cpf = data.map(item => item.cpf).reduce((acc, client) => {
-        acc += `<p>${client.cpf}</p>`
-        return acc
+      .then(function (data) {
+        let name = data.map(item => item.name).reduce((acc, client) => {
+          acc += `<p>${client.name}</p>`
+          return acc
+        })
+        name = name.split(' ')[0]
+        setName(name)
+        const cpf = data.map(item => item.cpf).reduce((acc, client) => {
+          acc += `<p>${client.cpf}</p>`
+          return acc
+        })
+        setCpf(cpf)
       })
-      setCpf(cpf)
-    })
   }, [])
 
 
@@ -38,7 +42,7 @@ export const Header = () => {
   return (
     <header className='header'>
       <div className='images-header'>
-        <div className='back'onClick={() => handleClick("/profile")}>
+        <div className={compareLocation ? "visibility" : "back"} onClick={() => handleClick("/profile")}>
           <Images src={back} alt='back' />
         </div>
         <div className='logo'>
